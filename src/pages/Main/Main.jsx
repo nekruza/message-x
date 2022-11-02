5b33ba97ec03ae3e09bd3d2929a80267ddc68544
Nekruz Avgani <nekruzavgani@Nekruzs-MacBook-Air.local>
2022-11-02 14:20:42 +0000

    cleanup

--- src/pages/Main/Main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./main.css";
-
-const Main = () => {
-    return <main className="Main">Main Stuff</main>;
-};
-export default Main;
--- src/pages/Sidebar/Sidebar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./sidebar.css";
-
-const Sidebar = () => {
-    return <aside className="Sidebar">Sidebar</aside>;
-};
-export default Sidebar;
