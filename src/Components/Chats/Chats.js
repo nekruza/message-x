1214b22731291a2018bf8f3401bcaebb763248d4
Nekruz Avgani <nekruzavgani@Nekruzs-MacBook-Air.local>
2022-11-02 14:27:44 +0000

    added component: Chats + styling

---
+++ src/Components/Chats/Chats.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Chats.css";
+
+const Chat = ({ message }) => {
+    const { text, is_user_msg } = message;
+    return (
+        <span className={`Chat ${is_user_msg ? "is-user-msg" : ""}`}
+        >{text}</span>
+    );
+};
+
+
+
+const Chats = ({ messages }) => {
+    const chatsRef = React.createRef();
+
+    const scrollToBottom = () => {
+        chatsRef.current.scrollTop =
+            chatsRef.current.scrollHeight;
+    };
+
+    React.useEffect(() => {
+        scrollToBottom()
+    }, [messages])
+
+    return (
+        <div className="Chats" ref={chatsRef}>
+            {messages.map(message => (
+                <Chat message={message} key={message.number} />
+            ))} </div>
+    );
+
+}
+export default Chats;
